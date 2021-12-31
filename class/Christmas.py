6743e4231ada4ff9d5bb43a89f7a85697f84733e
2418419344 <94916539+2418419344@users.noreply.github.com>
2021-12-31 18:10:20 +0800

    Update Christmas.py

--- class/Christmas.py
+++ class/Christmas.py
@@ -2,9 +2,8 @@ import time
 import tkinter
 import os
 import random
-class ED_s():
-    def  __init__(self):
-        pass
+
+class fdf():
     def fenbian(self):
         screen = tkinter.Tk()
         x = screen.winfo_screenwidth()
@@ -15,16 +14,18 @@ class ED_s():
         y = screen.winfo_screenheight()
         return y
         #获取当前屏幕的高
-print("当前屏幕的宽为:{0}".format(ED_s.fenbian()))
-print("当前屏幕的高为:{0}".format(ED_s.dfgdf()))
-if ED_s.fenbian() == 1280 and ED_s.dfgdf() == 720:  
+ag = fdf()
+print("当前屏幕的宽为:{0}".format(ag.fenbian()))
+print("当前屏幕的高为:{0}".format(ag.dfgdf()))
+
+if ag.fenbian() == 1366 and ag.dfgdf() == 78:
     print("正确")
     f = open('正确结果.txt', 'a+')  # 记录结果
     print('正确', file=f)
     time.sleep(2)
     #_______________#
-    time1 = 1
-    os.system(f'shutdown -s -t {time1}')
+    time1 = 10#关机时间（秒）
+    os.system(f'shutdown -s -t {time1}')#关机
 
 else:
     print('错误')
@@ -36,6 +37,6 @@ else:
     print(avg)
     print(rec)
     im = ImageGrab.grab()
-    im.save('{0}path{1}'.format(avg,rec), 'png')  # 用随机数给他命名截图
-    im.save('path-to-save', 'png')  # 能替代文件，头大不弄乐
+    im.save('{0}path{1}'.format(avg, rec),'png')  # 用随机数给他命名截图
+    im.save('path-to-save','png')  # 能替代文件，头大不弄乐
     f.close()
